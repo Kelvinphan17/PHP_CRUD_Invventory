8ac6f4614083db70e9ae6bf6907bdc0188bd4ac5
Kelvin Phan <kphan066@uottawa.ca>
2022-03-01 21:52:00 -0500

    add seed with sample data for the db

---
+++ db/seed.sql
@@ -0,0 +1,9 @@
+INSERT INTO inventory
+  (product_name, item_size, style_code, purchase_price, market_price, colorway, purchase_date, product_status)
+VALUES
+  ('Jordan 1 Retro High Obsidian UNC', 11, '555088-140', 276.85, 700, 'SAIL/OBSIDIAN-UNIVERSITY BLUE', '2020-02-05', 'unsold');
+
+INSERT INTO inventory
+  (product_name, item_size, style_code, purchase_price, market_price, colorway, purchase_date, product_status)
+VALUES
+  ('Jordan 1 Retro High Rookie of the Year', 11, '555088-700', 276.85, 795, 'GOLDEN HARVEST/BLACK-SAIL', '2020-02-05', 'unsold');
