dc1b1a82e9aed4c9ae0280146ad4ab0bbd5461dd
Kelvin Phan <kphan066@uottawa.ca>
2022-03-03 11:04:47 -0500

    modify formatting of text

--- bin/db/create.bat
+++ bin/db/create.bat
@@ -3,3 +3,4 @@ psql -U postgres -c "DROP DATABASE subspace" || true
 psql -U postgres -c "CREATE DATABASE subspace"
 psql -U postgres -d subspace -f ./db/schema.sql
 psql -U postgres -d subspace -f ./db/seed.sql
+
