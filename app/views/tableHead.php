e8e2e2cfec83b581314e817f552712bc0f038df8
Kelvin Phan <kphan066@uottawa.ca>
2022-04-04 12:35:25 -0400

    modify format of tablehead

--- app/views/tableHead.php
+++ app/views/tableHead.php
@@ -1,13 +1,13 @@
 <tr>
-                            <th>Name <button class="sort" data-id="product_name" data-order="" ><i class="fa-solid fa-sort"></i></button></th>
-                            <th>Size <button class="sort" data-id="item_size" data-order="" ><i class="fa-solid fa-sort"></i></th>
-                            <th><br>SKU</th>
-                            <th>Status <button class="sort" data-id="product_status" data-order="" ><i class="fa-solid fa-sort"></i></th>
-                            <th>Purchase Total <button class="sort" data-id="purchase_price" data-order="" ><i class="fa-solid fa-sort"></i></th>
-                            <th>Market Price <button class="sort" data-id="market_price" data-order="" ><i class="fa-solid fa-sort"></i></th>
-                            <th>Sold For <button class="sort" data-id="sold_price" data-order="" ><i class="fa-solid fa-sort"></i></th>
-                            <th>Profit <button class="sort" data-id="profit" data-order="" ><i class="fa-solid fa-sort"></i></th>
-                            <th><br>Colorway</th>
-                            <th>Date Purchased <button class="sort" data-id="purchase_date" data-order="" ><i class="fa-solid fa-sort"></i></th>
-                            <th><br>Actions</th>
-                        </tr>
+    <th>Name <button class="sort" data-id="product_name" data-order="" ><i class="fa-solid fa-sort"></i></button></th>
+    <th>Size <button class="sort" data-id="item_size" data-order="" ><i class="fa-solid fa-sort"></i></th>
+    <th><br>SKU</th>
+    <th>Status <button class="sort" data-id="product_status" data-order="" ><i class="fa-solid fa-sort"></i></th>
+    <th>Purchase Total <button class="sort" data-id="purchase_price" data-order="" ><i class="fa-solid fa-sort"></i></th>
+    <th>Market Price <button class="sort" data-id="market_price" data-order="" ><i class="fa-solid fa-sort"></i></th>
+    <th>Sold For <button class="sort" data-id="sold_price" data-order="" ><i class="fa-solid fa-sort"></i></th>
+    <th>Profit <button class="sort" data-id="profit" data-order="" ><i class="fa-solid fa-sort"></i></th>
+    <th><br>Colorway</th>
+    <th>Date Purchased <button class="sort" data-id="purchase_date" data-order="" ><i class="fa-solid fa-sort"></i></th>
+    <th><br>Actions</th>
+</tr>
